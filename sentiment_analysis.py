118f59088f100c3feb2b812be2b73c38f2a5e3df
Harsh Bansal <44253199+harsh74444@users.noreply.github.com>
2019-08-02 17:55:03 +0530

    Python code

---
+++ sentiment_analysis.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 29 10:55:03 2019
+
+@author: HarshGB
+"""
+import numpy as np
+import pandas as pd
+from collections import Counter
+import sklearn as skl
+import nltk.tokenize as token
+import matplotlib.pyplot as plt
+from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import logistic
+
+amazon = pd.read_csv(r"C:\Users\harshgb\Desktop\amazon_baby.csv", na_values = ' ')
+#print(amazon)
+amazon = pd.DataFrame(amazon)
+#print(amazon)
+print('\n')
+'''
+review = amazon['review']
+print(review)
+'''
+'''
+count = [Counter(review)]
+print(count)
+'''
+amazon['review'] = amazon['review'].fillna('NA')
+amazon['word_count'] = amazon['review'].apply(lambda x: Counter(x.split(' ')))
+#print(amazon['word_count'])
+#print(amazon.head())
+#plt.hist(amazon['name'])
+
+amazon = amazon[amazon['rating'] != 3]
+#print(amazon)
+
+amazon['sorted_rating'] = amazon['rating'].apply(lambda x: 1 if x > 3 else 0)
+
+X = amazon[['name', 'review', 'rating']]
+y = amazon['sorted_rating']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
